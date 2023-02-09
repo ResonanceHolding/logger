02bf13faa6dd8a18c2f16f1d59fae785648edcff
vladyslav <vladkukler@resonanceholding.com>
2023-02-09 13:51:46 +0200

    add sentry messages, update documentation

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ module.exports = {
 
   /**
    * Initialize logger (if wasn't called - logger will be initialized
-   * automatically with default parameters, such as logLevel = 'all', 
-   * throttleParams = { maxLength: 15, delay: 15 }). 
-   * 
+   * automatically with default parameters, such as logLevel = 'all',
+   * throttleParams = { maxLength: 15, delay: 15 }).
+   *
    * Initialization method can be called as Logger class method or via
    * functional wrapper (Init). Both of them are optional.
-   * 
+   *
    * All of the params are optional
    * @param {string} logLevel 'all' | 'error' | 'info' | 'debug'
    * @param {Sentry.NodeOptions} sentryConfig
@@ -25,8 +25,30 @@ module.exports = {
    * @param {*} tags object with tags name as keys and values as values
    */
   Init: (...args) => Logger.init(...args),
+
+  /**
+   * Prints pretty error to stderr and if Sentry initialized - sends error
+   * to Sentry
+   * @param  {...any} args
+   */
   Err: (...args) => Logger.err(...args),
+
+  /**
+   * Prints pretty message to stdout.
+   * @param  {...any} args
+   */
   Log: (...args) => Logger.log(...args),
+
+  /**
+   * Prints pretty message to stdout. If Sentry initialized - sends message
+   * to Sentry
+   * @param  {...any} args
+   */
   Debug: (...args) => Logger.debug(...args),
+
+  /**
+   * Prints pretty message to stdout.
+   * @param  {...any} args
+   */
   Info: (...args) => Logger.info(...args),
 };
--- readme.md
+++ readme.md
@@ -1,10 +1,11 @@
 # Logger
 
-Logger is exported as cached module, so you will have only 1 instance of logger in project. 
-Also module exports functional wrappers around logger methods, so you free to use it in functional way. 
+Logger is exported as cached module, so you will have only 1 instance of logger
+in project. Also module exports functional wrappers around logger methods, so
+you free to use it in functional way.
 
-You can specify log level by calling:
-Logger.init() or simply Init() and pass preferred level.
+You can specify log level by calling: Logger.init() or simply Init() and pass
+preferred level.
 
 Log level argument is case insensitive. Options are:
 
@@ -38,7 +39,8 @@ All constructor params are optional, such as init method call
 - `logLevel` - log level described above
 - `sentryConfig` - config for Sentry
   - `dsn` - dsn for Sentry project
-  - `environment` - environment for Sentry ('prod', 'test', 'dev', 'local', if 'local' provided - Sentry will be turned off)
+  - `environment` - environment for Sentry ('prod', 'test', 'dev', 'local', if
+    'local' provided - Sentry will be turned off)
 - `throttleParams`:
   - `maxLength` - max length of the map with last errors
   - `delay` - delay between displaying same error
